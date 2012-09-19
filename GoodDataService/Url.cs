6b926abd162c2f4811b47d5f544115f40358704a
Mike Matsui <mmatsui@groupcommerce.com>
2012-09-19 12:33:42 -0400

    DO NOT Merge - prototypting being able to edit user filters

---
+++ GoodDataService/Url.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using Newtonsoft.Json;
+
+namespace GoodDataService
+{
+	internal static class Url
+	{
+		public static string Combine(params string[] urlParts)
+		{
+			if (urlParts == null)
+				return null;
+
+			var sb = new StringBuilder();
+			foreach (var part in urlParts)
+			{
+				if (string.IsNullOrEmpty(part))
+					continue;
+				var trimmedPart = part.TrimEnd('/');
+				if (!string.IsNullOrEmpty(trimmedPart))
+				{
+					if (sb.Length > 0)
+					{
+						sb.Append("/");
+						trimmedPart = trimmedPart.TrimStart('/');
+					}
+
+					sb.Append(trimmedPart);
+				}
+			}
+			return sb.ToString();
+		}
+	}
+}
